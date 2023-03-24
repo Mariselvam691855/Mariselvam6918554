b7725184cea2d2419a87b5a5c4331baa5cda77eb
mariselvam <124579188+Mariselvam691855@users.noreply.github.com>
2023-03-24 23:07:25 +0530

    Add files via upload

---
+++ square_numbers.py
@@ -0,0 +1,20 @@
+class Point:
+    def __init__(self):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def sqsum(self):
+        return ((self.x **2), (self.y **2), (self.z **2))
+
+    def sum(self):
+        return ((self.x **2) + (self.y **2) + (self.z **2))
+
+x = int(input("Enter the first number :"))
+y = int(input("Enter the second number :"))
+z = int(input("Enter the third number :"))
+
+a = Point()
+
+print("square of the number are:",a.sqsum())
+print("sum of the square are :",a.sum())
